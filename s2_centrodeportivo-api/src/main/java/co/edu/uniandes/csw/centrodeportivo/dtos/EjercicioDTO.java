fadf17a9b98cca51bfd40afd5e4be79e5992f939
estudiante <estudiante@ISIS2603-360.sis.virtual.uniandes.edu.co>
2018-08-26 15:21:01 -0500

    Package dtos creado y clases movidas

---
+++ s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/dtos/EjercicioDTO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.centrodeportivo.dtos;
+
+/**
+ *aa
+ * @author estudiante
+ */
+public class EjercicioDTO 
+{
+    private Long id;
+    
+    private String name;
+    
+    public EjercicioDTO()
+    {
+        
+    }
+    
+    public EjercicioDTO(EjercicioEntity ejercicio)
+    {
+        
+    }
+
+    public EjercicioEntity toEntity()
+    {
+        return null;
+    }
+}
---
+++ s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/dtos/ZonaCuerpoDTO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.centrodeportivo.dtos;
+
+/**
+ *aa
+ * @author estudiante
+ */
+public class ZonaCuerpoDTO 
+{
+    
+    private Long id;
+    
+    private String name;
+    
+    public ZonaCuerpoDTO()
+    {
+        
+    }
+    
+    public ZonaCuerpoDTO(ZonaCuerpoEntity zonaDelCuerpo)
+    {
+        
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ZonaCuerpoEntity toEntity()
+    {
+        return null;
+    }
+    
+}
