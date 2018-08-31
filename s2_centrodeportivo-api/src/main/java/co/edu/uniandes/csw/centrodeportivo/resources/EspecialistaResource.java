ba8d5c8df20988eabd6421e6492372337cb7c298
Mac <fj.mac@uniandes.edu.co>
2018-08-31 09:32:10 -0500

    "especialista resource"

--- s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/resources/EspecialistaResource.java
+++ s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/resources/EspecialistaResource.java
@@ -5,10 +5,30 @@
  */
 package co.edu.uniandes.csw.centrodeportivo.resources;
 
+import co.edu.uniandes.csw.centrodeportivo.dtos.EspecialistaDTO;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author estudiante
  */
+@Path("especialistas")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class EspecialistaResource {
+     private static final Logger LOGGER = Logger.getLogger(EspecialistaResource.class.getName());
+
     
+     @POST
+    public EspecialistaDTO createCentroDeportivo(EspecialistaDTO especialista)
+    {
+        return especialista;
+    }
 }
