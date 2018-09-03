a129e3595c013fddc4181e865484d3d0f2bdfd99
Mac <fj.mac@uniandes.edu.co>
2018-09-03 08:16:21 -0500

    "especialista resource"

--- s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/dtos/EspecialistaDTO.java
+++ s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/dtos/EspecialistaDTO.java
@@ -5,14 +5,7 @@
  */
 package co.edu.uniandes.csw.centrodeportivo.dtos;
 
-import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
+
 
 /**
  *
