b1a974c2cc0876150f0df04cc4ee8e72693ddbce
Mac <fj.mac@uniandes.edu.co>
2018-09-03 08:32:49 -0500

    "especialista resource"

---
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/entities/EspecialistaEntity.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.centrodeportivo.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @authorFrancisco Jose MacAllister
+ */
+@Entity
+public class EspecialistaEntity implements Serializable {
+     private int id;
+    private String nombre;
+    private String especialidad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}
