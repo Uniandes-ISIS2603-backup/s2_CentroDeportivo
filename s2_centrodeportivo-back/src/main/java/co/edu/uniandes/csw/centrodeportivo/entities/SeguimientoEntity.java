ea568e220485e0c82a48263ea41ff669b48ac67f
estudiante <estudiante@ISIS2603-348.sis.virtual.uniandes.edu.co>
2018-09-03 08:45:39 -0500

    Seguimiento e Implemento Entity

---
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/entities/SeguimientoEntity.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.centrodeportivo.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Lina Cardozo
+ */
+@Entity
+public class SeguimientoEntity implements Serializable {
+    
+    private static final long serialVersionUID =1L;
+    
+    @Id
+    private Long id;
+    private Integer tiempo;
+    private Double calorias;
+    private Double velocidadPromedio;
+    private Double ritmoCardiacoPromedio;
+    
+    public SeguimientoEntity()
+    {
+        
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    
+    public Integer getTiempo()
+    {
+        return tiempo;
+    }
+    
+    public Double returnCalorias()
+    {
+        return calorias;
+    }
+    
+    public Double getVelocidadPromedio()
+    {
+        return velocidadPromedio;
+    }
+    
+    public Double getRitmoCardiacoPromedio()
+    {
+        return ritmoCardiacoPromedio;
+    }
+
+    public void setTiempo(Integer tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public void setCalorias(Double calorias) {
+        this.calorias = calorias;
+    }
+
+    public void setVelocidadPromedio(Double velocidadPromedio) {
+        this.velocidadPromedio = velocidadPromedio;
+    }
+
+    public void setRitmoCardiacoPromedio(Double ritmoCardiacoPromedio) {
+        this.ritmoCardiacoPromedio = ritmoCardiacoPromedio;
+    }
+    
+}
