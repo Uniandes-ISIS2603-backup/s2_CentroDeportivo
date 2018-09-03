893a4ba401165a766452ea452c40047e68698f60
estudiante <estudiante@ISIS2603-348.sis.virtual.uniandes.edu.co>
2018-09-03 11:35:14 -0500

    Persistencias 1

---
+++ s2_centrodeportivo-back/src/test/java/co/edu/uniandes/csw/s2_ludis/test/persistance/ImplementoPersistanceTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.s2_ludis.test.persistance;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author estudiante
+ */
+public class ImplementoPersistanceTest {
+    
+    public ImplementoPersistanceTest() {
+    }
+    
+    @Test
+    public void createImplementoTest()
+    {
+            
+    }
+    
+    @Deployment
+    public static JavaArchive createEmployment()
+    {
+        return null;
+        //Ver ejemplo para retornar el valor correcto
+    }
+}
---
+++ s2_centrodeportivo-back/src/test/java/co/edu/uniandes/csw/s2_ludis/test/persistance/SeguimientoPersistanceTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.s2_ludis.test.persistance;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author estudiante
+ */
+public class SeguimientoPersistanceTest {
+    
+    public SeguimientoPersistanceTest() {
+        
+    }
+    
+    @Test
+    public void createSeguimientoTest()
+    {
+            
+    }
+    
+    @Deployment
+    public static JavaArchive createEmployment()
+    {
+        return null;
+        //Ver ejemplo para retornar el valor correcto
+    }
+}
