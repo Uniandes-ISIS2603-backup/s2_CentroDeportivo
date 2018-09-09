8fc6ac54196cc43e9b9e345dee7e28085c7d6ee5
Mac <fj.mac@uniandes.edu.co>
2018-09-09 18:41:00 -0500

    Merge origin/master
    Conflicts:
    	s2_centrodeportivo-back/src/test/java/co/edu/uniandes/csw/ludis/test/persistence/EspecialistaPersistenceTest.java

--- s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/resources/EspecialistaResource.java
+++ s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/resources/EspecialistaResource.java
@@ -6,10 +6,15 @@
 package co.edu.uniandes.csw.centrodeportivo.resources;
 
 import co.edu.uniandes.csw.centrodeportivo.dtos.EspecialistaDTO;
+import co.edu.uniandes.csw.centrodeportivo.ejb.EspecialistaLogic;
+import co.edu.uniandes.csw.centrodeportivo.entities.EspecialistaEntity;
+import co.edu.uniandes.csw.centrodeportivo.exceptions.BusinessLogicException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -24,11 +29,23 @@ import javax.ws.rs.Produces;
 @RequestScoped
 public class EspecialistaResource {
      private static final Logger LOGGER = Logger.getLogger(EspecialistaResource.class.getName());
+ @Inject
+    EspecialistaLogic especialistaLogic;
+    
+    @POST
+    public EspecialistaDTO createEspecialista(EspecialistaDTO especialista) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "EspecialistaResource createEspecialista: input: {0}", especialista.toString());
+        // Convierte el DTO (json) en un objeto Entity para ser manejado por la lógica.
+        //EspecialistaEntity especialistaEntity = especialista.toEntity();
+        // Invoca la lógica para crear la editorial nueva
+        //EspecialistaEntity nuevoEspecialistaEntity = especialistaLogic.createEpecialista(especialistaEntity);
+        // Como debe retornar un DTO (json) se invoca el constructor del DTO con argumento el entity nuevo
+        //EspecialistaDTO nuevoEspecialistaDTO = new EspecialistaDTO(nuevoEspecialistaEntity);
+        //LOGGER.log(Level.INFO, "EditorialResource createEditorial: output: {0}", nuevoEditorialDTO.toString());
+        return null;
+    }
 
     
-     @POST
-    public EspecialistaDTO createCentroDeportivo(EspecialistaDTO especialista)
-    {
-        return especialista;
-    }
+    
+    
 }
--- s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/persistence/EspecialistaPersistence.java
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/persistence/EspecialistaPersistence.java
@@ -6,7 +6,6 @@
 package co.edu.uniandes.csw.centrodeportivo.persistence;
 
 import co.edu.uniandes.csw.centrodeportivo.entities.EspecialistaEntity;
-import co.edu.uniandes.csw.centrodeportivo.entities.ObjetivoEntity;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,12 +26,12 @@ public class EspecialistaPersistence {
     protected EntityManager em;
     
      public EspecialistaEntity create(EspecialistaEntity especialistaEntity) {
-        LOGGER.log(Level.INFO, "Creando un objetivo nuevo");
+        LOGGER.log(Level.INFO, "Creando un nuevo especialista");
         /* Note que hacemos uso de un método propio de EntityManager para persistir al deportista en la base de datos.
         Es similar a "INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);" en SQL.
          */
         em.persist(especialistaEntity);
-        LOGGER.log(Level.INFO, "Saliendo de crear un objetivo nuevo");
+        LOGGER.log(Level.INFO, "Saliendo de crear un nuevo especialista");
         return especialistaEntity;
     }
      
@@ -44,7 +43,7 @@ public class EspecialistaPersistence {
         return query.getResultList();
     }
        public EspecialistaEntity find(Long especialistaId) {
-        LOGGER.log(Level.INFO, "Consultando el objetivo con id={0}", especialistaId);
+        LOGGER.log(Level.INFO, "Consultando el especialista con id={0}", especialistaId);
         /* Note que se hace uso del metodo "find" propio del EntityManager, el cual recibe como argumento 
         el tipo de la clase y el objeto que nos hara el filtro en la base de datos en este caso el "id"
         Suponga que es algo similar a "select * from ObjetivoEntity where id=id;" - "SELECT * FROM table_name WHERE condition;" en SQL.
@@ -57,7 +56,38 @@ public class EspecialistaPersistence {
         el deportista con los cambios, esto es similar a 
         "UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
          */
-        LOGGER.log(Level.INFO, "Saliendo de actualizar el objetivo con id = {0}", especialistaEntity.getId());
+        LOGGER.log(Level.INFO, "Saliendo de actualizar el especialista con id = {0}", especialistaEntity.getId());
         return em.merge(especialistaEntity);
+    }
+
+     public void delete(Long especialistaId) {
+        LOGGER.log(Level.INFO, "Borrando especialista con id = {0}", especialistaId);
+        // Se hace uso de mismo método que esta explicado en public EditorialEntity find(Long id) para obtener la editorial a borrar.
+        EspecialistaEntity entity = em.find(EspecialistaEntity.class, especialistaId);
+        /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
+         EntityManager para eliminar de la base de datos el objeto que encontramos y queremos borrar.
+         Es similar a "delete from EditorialEntity where id=id;" - "DELETE FROM table_name WHERE condition;" en SQL.*/
+        em.remove(entity);
+        LOGGER.log(Level.INFO, "Saliendo de borrar el especialista con id = {0}", especialistaId);
+    }
+
+   public EspecialistaEntity findByName(String name) {
+        LOGGER.log(Level.INFO, "Consultando especialista por nombre ", name);
+        // Se crea un query para buscar editoriales con el nombre que recibe el método como argumento. ":name" es un placeholder que debe ser remplazado
+        TypedQuery query = em.createQuery("Select e From EspecialistaEntity e where e.name = :name", EspecialistaEntity.class);
+        // Se remplaza el placeholder ":name" con el valor del argumento 
+        query = query.setParameter("name", name);
+        // Se invoca el query se obtiene la lista resultado
+        List<EspecialistaEntity> sameName = query.getResultList();
+        EspecialistaEntity result;
+        if (sameName == null) {
+            result = null;
+        } else if (sameName.isEmpty()) {
+            result = null;
+        } else {
+            result = sameName.get(0);
+        }
+        LOGGER.log(Level.INFO, "Saliendo de consultar especialista por nombre ", name);
+        return result;
     }
 }
