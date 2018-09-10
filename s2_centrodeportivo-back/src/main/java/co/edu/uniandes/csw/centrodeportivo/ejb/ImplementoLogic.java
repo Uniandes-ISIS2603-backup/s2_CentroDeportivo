d13306ae502dbd2b9c97e355173f85286e683d42
Lina Cardozo <lp.cardozo@uniandes.edu.co>
2018-09-10 12:27:04 -0500

    Lógica Seguimiento e Implemento

--- s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/ImplementoLogic.java
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/ImplementoLogic.java
@@ -6,6 +6,12 @@
 package co.edu.uniandes.csw.centrodeportivo.ejb;
 
 import co.edu.uniandes.csw.centrodeportivo.entities.ImplementoEntity;
+import co.edu.uniandes.csw.centrodeportivo.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.centrodeportivo.persistence.ImplementoPersistence;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.util.List;
+import java.util.logging.Level;
+import javax.inject.Inject;
 
 /**
  *
@@ -13,12 +19,91 @@ import co.edu.uniandes.csw.centrodeportivo.entities.ImplementoEntity;
  */
 public class ImplementoLogic {
 
-    public static ImplementoEntity createImplemento(ImplementoEntity implementoEntity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Inject
+    private ImplementoPersistence persistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
+
+    /**
+     * Crea un implemento en la persistencia.
+     *
+     * @param implementoEntity La entidad que representa el implemento a
+     * persistir.
+     * @return La entiddad del implemento luego de persistirla.
+     * @throws BusinessLogicException Si el implemento a persistir ya existe.
+     */
+    public ImplementoEntity createImplemento(ImplementoEntity implementoEntity) throws BusinessLogicException 
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de creación del implemento");
+        // Invoca la persistencia para crear el implemento
+        persistence.create(implementoEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de creación del implemento");
+        return implementoEntity;
     }
 
+    /**
+     *
+     * Obtener todos los implementos existentes en la base de datos.
+     *
+     * @return una lista de implementos.
+     */
+    public List<ImplementoEntity> getImplementos() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los implementos");
+        // Note que, por medio de la inyección de dependencias se llama al método "findAll()" que se encuentra en la persistencia.
+        List<ImplementoEntity> implementos = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todos los implementoes");
+        return implementos;
+    }
+
+    /**
+     *
+     * Obtener un implemento por medio de su id.
+     *
+     * @param implementosId: id del implemento para ser buscado.
+     * @return el implemento solicitado por medio de su id.
+     */
     public ImplementoEntity getImplemento(Long implementosId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar la implemento con id = {0}", implementosId);
+        // Note que, por medio de la inyección de dependencias se llama al método "find(id)" que se encuentra en la persistencia.
+        ImplementoEntity implementoEntity = persistence.find(implementosId);
+        if (implementoEntity == null) {
+            LOGGER.log(Level.SEVERE, "El implemento con el id = {0} no existe", implementosId);
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de consultar el implemento con id = {0}", implementosId);
+        return implementoEntity;
+    }
+
+    /**
+     *
+     * Actualizar un implemento.
+     *
+     * @param implementosId: id del implemento para buscarlo en la base de
+     * datos.
+     * @param implementoEntity: implemento con los cambios para ser actualizado,
+     * por ejemplo el .
+     * @return el implemento con los cambios actualizados en la base de datos.
+     */
+    public ImplementoEntity updateImplemento(Long implementosId, ImplementoEntity implementoEntity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar el implemento con id = {0}", implementosId);
+        // Note que, por medio de la inyección de dependencias se llama al método "update(entity)" que se encuentra en la persistencia.
+        ImplementoEntity newEntity = persistence.update(implementoEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar el implemento con id = {0}", implementoEntity.getId());
+        return newEntity;
+    }
+
+    /**
+     * Borrar un implemento
+     *
+     * @param implementosId: id del implemento a borrar
+     * @throws BusinessLogicException Si el implemento a eliminar tiene un ejercicio asociado.
+     */
+    public void deleteImplemento(Long implementosId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar el implemento con id = {0}", implementosId);
+        // Note que, por medio de la inyección de dependencias se llama al método "delete(id)" que se encuentra en la persistencia.
+        ImplementoEntity implementoEntity = persistence.find(implementosId);
+        if (implementoEntity.getEjercicio() != null) {
+            throw new BusinessLogicException("No se puede borrar el implemento con id = " + implementosId + " porque tiene un ejercicio asociado");
+        }
+        persistence.delete(implementosId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar el implemento con id = {0}", implementosId);
     }
     
 }
--- s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/SeguimientoLogic.java
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/SeguimientoLogic.java
@@ -5,10 +5,111 @@
  */
 package co.edu.uniandes.csw.centrodeportivo.ejb;
 
+import co.edu.uniandes.csw.centrodeportivo.entities.MaquinaEntity;
+import co.edu.uniandes.csw.centrodeportivo.entities.SeguimientoEntity;
+import co.edu.uniandes.csw.centrodeportivo.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.centrodeportivo.persistence.SeguimientoPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+
 /**
  *
  * @author estudiante
  */
 public class SeguimientoLogic {
+    
+    private static final Logger LOGGER = Logger.getLogger(SeguimientoLogic.class.getName());
+
+    @Inject
+    private SeguimientoPersistence persistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
+
+    /**
+     * Crea un seguimiento en la persistencia.
+     *
+     * @param seguimientoEntity La entidad que representa el seguimiento a
+     * persistir.
+     * @return La entiddad del seguimiento luego de persistirla.
+     * @throws BusinessLogicException Si el seguimiento a persistir ya existe.
+     */
+    public SeguimientoEntity createSeguimiento(SeguimientoEntity seguimientoEntity) throws BusinessLogicException 
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de creación del seguimiento");
+        // Invoca la persistencia para crear el seguimiento
+        persistence.create(seguimientoEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de creación del seguimiento");
+        return seguimientoEntity;
+    }
+
+    /**
+     *
+     * Obtener todos los seguimientos existentes en la base de datos.
+     *
+     * @return una lista de seguimientos.
+     */
+    /*public List<SeguimientoEntity> getSeguimientos() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los seguimientos");
+        // Note que, por medio de la inyección de dependencias se llama al método "findAll()" que se encuentra en la persistencia.
+        List<SeguimientoEntity> seguimientos = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todas las seguimientoes");
+        return seguimientos;
+    }*/
+
+    /**
+     *
+     * Obtener un seguimiento por medio de su id.
+     *
+     * @param seguimientosId: id del seguimiento para ser buscado.
+     * @return el seguimiento solicitado por medio de su id.
+     */
+    public SeguimientoEntity getSeguimiento(Long seguimientosId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar la seguimiento con id = {0}", seguimientosId);
+        // Note que, por medio de la inyección de dependencias se llama al método "find(id)" que se encuentra en la persistencia.
+        SeguimientoEntity seguimientoEntity = persistence.find(seguimientosId);
+        if (seguimientoEntity == null) {
+            LOGGER.log(Level.SEVERE, "El seguimiento con el id = {0} no existe", seguimientosId);
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de consultar el seguimiento con id = {0}", seguimientosId);
+        return seguimientoEntity;
+    }
+
+    /**
+     *
+     * Actualizar un seguimiento.
+     *
+     * @param seguimientosId: id del seguimiento para buscarlo en la base de
+     * datos.
+     * @param seguimientoEntity: seguimiento con los cambios para ser actualizado,
+     * por ejemplo el tiempo.
+     * @return el seguimiento con los cambios actualizados en la base de datos.
+     */
+    public SeguimientoEntity updateSeguimiento(Long seguimientosId, SeguimientoEntity seguimientoEntity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar el seguimiento con id = {0}", seguimientosId);
+        // Note que, por medio de la inyección de dependencias se llama al método "update(entity)" que se encuentra en la persistencia.
+        SeguimientoEntity newEntity = persistence.update(seguimientoEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar el seguimiento con id = {0}", seguimientoEntity.getId());
+        return newEntity;
+    }
+
+    /**
+     * Borrar un seguimiento
+     *
+     * @param seguimientosId: id del seguimiento a borrar
+     * @throws BusinessLogicException Si el seguimiento a eliminar tiene libros.
+     */
+    public void deleteSeguimiento(Long seguimientosId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar el seguimiento con id = {0}", seguimientosId);
+        // Note que, por medio de la inyección de dependencias se llama al método "delete(id)" que se encuentra en la persistencia.
+        List<MaquinaEntity> maquinas = getSeguimiento(seguimientosId).getMaquinas();
+        if (maquinas != null && !maquinas.isEmpty()) {
+            throw new BusinessLogicException("No se puede borrar el autor con id = " + seguimientosId + " porque tiene máquinas asociadas");
+        }
+        if (persistence.find(seguimientosId).getDeportista() != null) {
+            throw new BusinessLogicException("No se puede borrar el premio con id = " + seguimientosId + " porque tiene un deportista asociado");
+        }
+        persistence.delete(seguimientosId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar el seguimiento con id = {0}", seguimientosId);
+    }
     
 }
