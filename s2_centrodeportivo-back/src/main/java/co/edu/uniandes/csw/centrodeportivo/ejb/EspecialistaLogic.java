f5641e966f2bb55cb74606b41f9f23799a222e8d
Mac <fj.mac@uniandes.edu.co>
2018-09-13 08:33:29 -0500

    Arreglo logica stateless

--- s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/EspecialistaLogic.java
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/EspecialistaLogic.java
@@ -10,12 +10,14 @@ import co.edu.uniandes.csw.centrodeportivo.persistence.EspecialistaPersistence;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
  *
  * @author Francisco Jose MacAllister
  */
+@Stateless
 public class EspecialistaLogic {
 
  private static final Logger LOGGER = Logger.getLogger(EspecialistaLogic.class.getName());
--- s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/RutinaLogic.java
+++ s2_centrodeportivo-back/src/main/java/co/edu/uniandes/csw/centrodeportivo/ejb/RutinaLogic.java
@@ -10,12 +10,14 @@ import co.edu.uniandes.csw.centrodeportivo.persistence.RutinaPersistence;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
  *
  * @author Francisco Jose MacAllister
  */
+@Stateless
 public class RutinaLogic {
     private static final Logger LOGGER = Logger.getLogger(RutinaLogic.class.getName());
     
