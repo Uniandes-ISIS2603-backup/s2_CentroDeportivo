f0406af16df8e050762705afe101f94813d2e594
estudiante <dy.quintero@uniandes.edu.co>
2018-10-23 20:55:17 -0500

    CORSFilter modificado

--- s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/filters/CORSFilter.java
+++ s2_centrodeportivo-api/src/main/java/co/edu/uniandes/csw/centrodeportivo/filters/CORSFilter.java
@@ -6,17 +6,17 @@
 package co.edu.uniandes.csw.centrodeportivo.filters;
 
 import java.io.IOException;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 
-/**
- *
- * @author estudiante
- */
-public class CORSFilter implements ContainerResponseFilter{
-    
-     public void filter(final ContainerRequestContext requestContext,
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+   @Override
+   public void filter(final ContainerRequestContext requestContext,
                       final ContainerResponseContext cres) throws IOException {
       cres.getHeaders().add("Access-Control-Allow-Origin", "*");
       cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
@@ -24,4 +24,5 @@ public class CORSFilter implements ContainerResponseFilter{
       cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
       cres.getHeaders().add("Access-Control-Max-Age", "1209600");
    }
+
 }
